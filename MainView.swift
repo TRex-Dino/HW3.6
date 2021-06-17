e52843fa60a7a4b6eb43ded3d21e4e0009d28c11
Dima <dmitrysinkov6@gmail.com>
2021-06-17 22:05:18 +0300

    offset

--- MainView.swift
+++ MainView.swift
@@ -12,31 +12,30 @@ struct MainView: View {
     
     var body: some View {
         VStack {
-            Button(action : { showAward.toggle() }) {
+            Button(action : buttonAction) {
                 HStack {
                     Text(showAward ? "Hide Award": "Show Award")
                     Spacer()
                     Image(systemName: "chevron.up.square")
                         .rotationEffect(.degrees(showAward ? 0 : 180))
-                        .animation(.default)
+//                        .animation(.default)
                     
                 }
             }
             Spacer()
             GradientRec(width: 250, height: 250)
                 .offset(x: showAward ? 0 : -UIScreen.main.bounds.width)
-                .animation(
-                    .spring(
-                        response: 0.55,
-                        dampingFraction: 0.45,
-                        blendDuration: 0
-                    )
-                
-                )
+//                .animation(Animation.spring())
             Spacer()
         }
         .font(.headline)
         .padding()
+    }
+    
+    private func buttonAction() {
+        withAnimation {
+            showAward.toggle()
+        }
     }
 }
 
