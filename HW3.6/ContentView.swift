ffc44bc291ec94a324417d95bfba99fae131b742
Dima <dmitrysinkov6@gmail.com>
2021-06-17 20:38:13 +0300

    Initial Commit

---
+++ HW3.6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.6
+//
+//  Created by Dmitry on 17.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.6/HW3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_6App.swift
+//  HW3.6
+//
+//  Created by Dmitry on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
