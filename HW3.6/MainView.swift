795aa2d5a6c48b10deccf98ab629d5ed2f8f549e
Dima <dmitrysinkov6@gmail.com>
2021-06-18 12:06:50 +0300

    add color

--- HW3.6/MainView.swift
+++ HW3.6/MainView.swift
@@ -11,27 +11,32 @@ struct MainView: View {
     @State private var showAward = false
     
     var body: some View {
-        VStack {
-            Button(action : buttonAction) {
-                HStack {
-                    Text(showAward ? "Hide Award": "Show Award")
-                    Spacer()
-                    Image(systemName: "chevron.up.square")
-                        .scaleEffect(showAward ? 2 : 1)
-                        .rotationEffect(.degrees(showAward ? 0 : 180))
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea(.all)
+            VStack {
+                Button(action : buttonAction) {
+                    HStack {
+                        Text(showAward ? "Hide Award": "Show Award")
+                        Spacer()
+                        Image(systemName: "chevron.up.square")
+                            .scaleEffect(showAward ? 2 : 1)
+                            .rotationEffect(.degrees(showAward ? 0 : 180))
+                    }
+                    .foregroundColor(.yellow)
                 }
+                Spacer()
+                
+                if showAward {
+                    RabbitView(width: 200, height: 200)
+                        .transition(.transition)
+                }
+                
+                Spacer()
             }
-            Spacer()
-            
-            if showAward {
-                RabbitView(width: 200, height: 200)
-                    .transition(.transition)
-            }
-            
-            Spacer()
+            .font(.headline)
+            .padding()
         }
-        .font(.headline)
-        .padding()
     }
     
     private func buttonAction() {
